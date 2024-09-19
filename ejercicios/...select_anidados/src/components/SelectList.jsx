e3ca95ccb9073d4b52de20a14df6b976fccf9448
JULY <danielitha345ra@gmail.com>
2024-09-19 08:50:03 -0500

    inicio de trabajo de select para informacion de la ciudad aun no esta terminado

--- ejercicios/...select_anidados/src/components/SelectList.jsx
+++ ejercicios/...select_anidados/src/components/SelectList.jsx
@@ -1,12 +1,33 @@
-export const SelectList = ({ title }) => {
-  const Key = `select-${title}`
-  const label =title.toUpperCase(); /**el toUpperCase lo pone todo en mayusculas */
-  const option = [];
+import { useFetch } from "../hooks/useFetch";
+
+export const SelectList = ({ manejadorCambio, title, url }) => {
+  
+  const Key = `select-${title}`;
+  const label = title.toUpperCase(); /**el toUpperCase lo pone todo en mayusculas */
+  const { data, error, loading}= useFetch (url);
+  
+  if(!data){
+    return null
+  } 
+  if(error){
+    return (<p> Ha ocurrido un error: {error}</p>)
+  }
+  
+  let options = data 
+  
   return (
     < section className="seccion1">
-      <label className="labels" htmlFor={Key}>{label}</label>
-      <select className="selects" name={Key} id={Key}>
-        <option value="">Seleccione un(a) {label}</option>
+      <label className="labels" htmlFor={Key}>
+        {label}
+        </label>
+      <select className="selects" name={Key} id={Key} onChange={manejadorCambio}>
+        <option value="">Seleccione un(a) {title}</option>
+      {data && options.map((option)=>(
+        <option key={option.id} value={option.id}>{option.name}</option>
+      ))}
+
+      
+
       </select>
     </section>
   );
--- ejercicios/...select_anidados/src/components/Selects.jsx
+++ ejercicios/...select_anidados/src/components/Selects.jsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
 import { SelectList } from "./SelectList";
 
 export const Selects = () => {
+  const [departamentos, setDepartamentos] = useState("");
+  const [ciudades, setCiudades] = useState("");
+  const [informacion, setInformacion] = useState(null);
   return (
     <>
-      <SelectList title = "Estado" />
-      <SelectList title = "Municipio" />
-      <SelectList title = "Ciudad" />
+      <SelectList
+        title="Departamentos"
+        url="https://api-colombia.com/api/v1/Department"
+        manejadorCambio={(event) => {
+          setDepartamentos(event.target.value);
+        }}
+      />
+      <SelectList 
+     title="Ciudad" 
+     url={`https://api-colombia.com/api/v1/Department/${departamentos}/cities`}
+     manejadorCambio={(event) => {
+      setCiudades(event.target.value);
+     }}
+    />
+      
+      <div className="Informacion" > 
+    <h3>Informacion de la ciudad</h3>
+    
+    <div url={`https://api-colombia.com/api/v1/Department/${ciudades}`} ></div>
+      </div>
     </>
   );
 };
