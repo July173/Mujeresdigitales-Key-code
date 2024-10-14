cd39581809978a9e46bbe029cc3aedcf4cba7c6c
JULY <danielitha345ra@gmail.com>
2024-10-14 13:29:34 -0500

    section welcome

--- ejercicios/proyect-tailwind/src/App.jsx
+++ ejercicios/proyect-tailwind/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import Welcome from './components/welcome'
 
 function App() {
 
   return (
     <>
      <Navbar/>
+     <Welcome/>
     </>
   )
 }
